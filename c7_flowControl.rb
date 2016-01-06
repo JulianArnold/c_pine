9dd2f25e9eb278025acafdcc31cf205d63f5395b
Julian Arnold <julian_arnold@hotmail.com>
2016-01-06 21:59:23 +0000

    new adds for the year include arrays & iterators practice

--- c7_flowControl.rb
+++ c7_flowControl.rb
@@ -211,21 +211,21 @@
 
 # 20151124 - 20151205: leap years: reps: 30 inclusive
 
-# puts 'Pick a starting year (like 1973 or something):'
-# starting = gets.chomp.to_i
-#
-# puts 'Now pick an ending year:'
-# ending = gets.chomp.to_i
-#
-# puts 'Check it out, these are leap years:'
-#
-# year = starting
-#
-# while year <= ending
-#   if year%4 == 0
-#     if year%100 != 0 || year%400 == 0
-#       puts year
-#     end
-#   end
-#   year += 1
-# end
+puts 'Pick a starting year (like 1973 or something):'
+starting = gets.chomp.to_i
+
+puts 'Now pick an ending year:'
+ending = gets.chomp.to_i
+
+puts 'Check it out, these are leap years:'
+
+year = starting
+
+while year <= ending
+  if year%4 == 0
+    if year%100 != 0 || year%400 == 0
+      puts year
+    end
+  end
+  year += 1
+end
--- sample.rb
+++ sample.rb
@@ -1,13 +1,3 @@
-# 20151130 - 20151208: Arrays & Iterators
+# 20151130 - 20160106: Arrays & Iterators
 
-# Reps: 3
-
-languages = ['Norwegian', 'English', 'Ruby']
-
-languages.each do |lang|
-  puts 'I love '+lang+''
-  puts 'Don\'t you?'
-end
-
-puts 'And let\'s hear it for Java!!!'
-puts '<crickets chirp in the distance>'
+# Reps: 17
