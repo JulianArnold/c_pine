8c200425f792c4cb760ad1f106c27a55b32fa327
Julian Arnold <julian_arnold@hotmail.com>
2016-03-26 18:10:59 +0000

    working on writing your own methods

--- sample.rb
+++ sample.rb
@@ -1,13 +1,3 @@
-# 20160301 - 20160314: Writing your own methods
+# 20160301 - 20160326: Writing your own methods
 
-# reps: 13
-
-tough_var = 'You can\'t even touch my variable!'
-
-def little_pest tough_var
-  tough_var = nil
-  puts 'oops!  I broke your variable!'
-end
-
-little_pest tough_var
-puts tough_var
+# reps: 10
